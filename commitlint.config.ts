c653492b590ed9c489bdc773c19788d269c240d2
dhinakaran7501 <dhinakings123@gmail.com>
2025-03-06 14:57:07 +0530

    setup husky

---
+++ commitlint.config.ts
@@ -0,0 +1,21 @@
+import type { UserConfig } from '@commitlint/types';
+
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  parserPreset: 'conventional-changelog-atom',
+  formatter: '@commitlint/format',
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert'],
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      ['setup', 'config', 'deps', 'feature', 'bug', 'docs', 'style', 'refactor', 'test', 'build', 'ci', 'release', 'other'],
+    ],
+  },
+};
+
+export default Configuration;
